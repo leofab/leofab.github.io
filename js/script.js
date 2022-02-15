67e5ce9094c25d12a4459c407217eedc91f71cfe
Leo Fabricio <leofabr3@gmail.com>
2022-02-14 22:10:48 -0300

    smoothscroll refact

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 // Smooth scrolling animation
 
+const about = document.querySelector('#sobre');
+const aboutBtn = document.querySelector('.about');
+const aboutBtn2 = document.querySelector('.about2');
+
+aboutBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+  about.scrollIntoView({
+    behavior: "smooth"
+  })
+})
+aboutBtn2.addEventListener('click', (e) => {
+  e.preventDefault()
+  about.scrollIntoView({
+    behavior: "smooth"
+  })
+})
